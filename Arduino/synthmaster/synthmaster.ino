b3c13c7af7ca0e39b5bce8a9dc5a9fa402d4af90
staplejm <staplejm@dukes.jmu.edu>
2017-08-03 09:04:57 -0400

    Test this!

--- Arduino/synthmaster/synthmaster.ino
+++ Arduino/synthmaster/synthmaster.ino
@@ -6,7 +6,7 @@ int previousCounter = 1;
 long previousMillis = 0;
 boolean samplePlayed = false;
 
-int switchStatus[] =  {1,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0};
+int switchStatus[] =  {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
 int switchChannel[] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
 int switchTiming[] =  {1,  1,  1,  1,  2,  2,  2,  2,  3,  3,  3,  3,  4,  4,  4,  4};
 
@@ -14,10 +14,9 @@ long interval = 1000;
 
 void setup() {
   pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
   delay(150);
-  //Wire.begin(5);
-  //Wire.onReceive(receiveEvent);
+  Wire.begin(5);
+  Wire.onReceive(receiveEvent);
   delay(1000);
 }
 
@@ -32,17 +31,12 @@ void loop() {
       counter = previousCounter + 1;
     }
   }
-  if(counter == 1){
-    digitalWrite(13, HIGH);
-  } else {
-    digitalWrite(13, LOW);
-  }
-  
+
   
   //PLAY SAMPLES
   for(int i=0; i<17; i++){
-    if(switchStatus[i] > 0) {
-      if(counter == switchTiming[i]){  //add && samplePlayed == false to statement after LED testing
+    if(counter == switchTiming[i] && samplePlayed == false) {
+      if(switchStatus[i] > 0) {
         //play audio sample
         digitalWrite(13, HIGH);
         samplePlayed = true;
@@ -52,8 +46,8 @@ void loop() {
       }
     }
   }
-
-/*void receiveEvent (int howMany) {
+}
+void receiveEvent (int howMany) {
   while(Wire.available() > 0) {
     char messageType = Wire.receive();
     int n = Wire.receive();
@@ -65,24 +59,22 @@ void loop() {
       delay(5);
     }
     if(messageType == 'n' && midiStatus == false) {
-      for(i=0; i<17; i++){
+      for(int i=0; i<17; i++){
         if(switchChannel[i] = n){
-          switchStatus = 1;
+          switchStatus[i] = 1;
         }
       }
       delay(5);
     }
-    if(messageType == 'o' && midiStatus == true) {
-      for(i=0; i<17; i++){
+    if(messageType == 'o' && midiStatus == false) {
+      for(int i=0; i<17; i++){
         if(switchChannel[i] = n){
-          switchStatus = 0;
+          switchStatus[i] = 0;
         }
       }
-      delay(5);
-    }
     delay(5);
+  }
   } 
   usbMIDI.send_now();
-*/
 }
 

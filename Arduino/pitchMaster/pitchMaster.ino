7d8917d3c51504435742a455d73801b6f11be5d8
staplejm <staplejm@dukes.jmu.edu>
2017-08-09 21:30:38 -0400

    These still don't work.

--- Arduino/pitchMaster/pitchMaster.ino
+++ Arduino/pitchMaster/pitchMaster.ino
@@ -1,53 +1,72 @@
-//Master Tile
+#include <Audio.h>
 #include <Wire.h>
+
+
+// GUItool: begin automatically generated code
+AudioSynthWaveform       waveform2;      //xy=162.00000762939453,186.00000524520874
+AudioSynthWaveform       waveform1;      //xy=163,114
+AudioSynthWaveform       waveform3;      //xy=167,254
+AudioSynthWaveform       waveform4;      //xy=171,323
+AudioSynthWaveform       waveform6;      //xy=174.00000381469727,444.00001335144043
+AudioSynthWaveform       waveform5;      //xy=175.00000381469727,389.0000114440918
+AudioSynthWaveform       waveform8;      //xy=178.00000762939453,562.0000171661377
+AudioSynthWaveform       waveform7;      //xy=181.00000762939453,499.0000147819519
+AudioMixer4              mixer1;         //xy=504.0000648498535,166.00001907348633
+AudioMixer4              mixer2;         //xy=535,416
+AudioMixer4              mixer3;         //xy=737,278
+AudioOutputAnalog        dac1;           //xy=917.0000267028809,277.00000858306885
+AudioConnection          patchCord1(waveform2, 0, mixer1, 1);
+AudioConnection          patchCord2(waveform1, 0, mixer1, 0);
+AudioConnection          patchCord3(waveform3, 0, mixer1, 2);
+AudioConnection          patchCord4(waveform4, 0, mixer1, 3);
+AudioConnection          patchCord5(waveform6, 0, mixer2, 1);
+AudioConnection          patchCord6(waveform5, 0, mixer2, 0);
+AudioConnection          patchCord7(waveform8, 0, mixer2, 3);
+AudioConnection          patchCord8(waveform7, 0, mixer2, 2);
+AudioConnection          patchCord9(mixer1, 0, mixer3, 0);
+AudioConnection          patchCord10(mixer2, 0, mixer3, 1);
+AudioConnection          patchCord11(mixer3, dac1);
+// GUItool: end automatically generated code
+
 
 int address = 5;                //all tiles must have unique address
 boolean midiStatus = true;
 
-
-/*SINUS FADE
-int value, value2, value3 ;
-int ledpin = 6;                           // light connected to digital pin 10
-int ledpin2 = 4; 
-int ledpin3 = 3;
-int ledpin4 = 9;
-int ledpin5 = 10;
-int ledpin6 = 16;
-
-long time=0;
-
-int periode = 2500;
-int displace = 500;
-int displaceTwo = 1000;
-*/
-
 void setup() {
   pinMode(13, OUTPUT);
+  dac1.analogReference(EXTERNAL);
+  waveform1.begin(0.4, 200, WAVEFORM_SAWTOOTH);
+  waveform2.begin(0.4, 200, WAVEFORM_SAWTOOTH);
+  waveform3.begin(0.4, 200, WAVEFORM_SAWTOOTH);
+  waveform4.begin(0.4, 200, WAVEFORM_SAWTOOTH);
+  waveform5.begin(0.4, 200, WAVEFORM_SAWTOOTH);
+  waveform6.begin(0.4, 200, WAVEFORM_SAWTOOTH);
+  waveform7.begin(0.4, 200, WAVEFORM_SAWTOOTH);
+  waveform8.begin(0.4, 200, WAVEFORM_SAWTOOTH);
+  mixer1.gain(0, 0.75);
+  mixer1.gain(1, 0.75);
+  mixer1.gain(2, 0.75);
+  mixer1.gain(3, 0.75);
+  mixer2.gain(0, 0.75);
+  mixer2.gain(1, 0.75);
+  mixer2.gain(2, 0.75);
+  mixer2.gain(3, 0.75);
+  mixer3.gain(0, 0.75);
+  mixer3.gain(1, 0.75);
   digitalWrite(13, HIGH);
   delay(150);
-  Wire.begin(5);
-  Wire.onReceive(receiveEvent);
+  //Wire.begin(5);
+  //Wire.onReceive(receiveEvent);
   delay(1000);
+  
 
 }
 
 void loop() {
- /*  
- COLOR CYCLE CODE
- time = millis();
- value = 128+127*cos(2*PI/periode*time);
- value2 = 128+127*cos(2*PI/periode*(displace-time));
- value3 = 128+127*cos(2*PI/periode*(displaceTwo-time));
- analogWrite(ledpin, value);           // sets the value (range from 0 to 255) 
- analogWrite(ledpin2, value2);           // sets the value (range from 0 to 255) 
- analogWrite(ledpin3, value3);
- analogWrite(ledpin4, value);
- analogWrite(ledpin5, value2);
- analogWrite(ledpin6, value3);
-*/
+
 }
 
-void receiveEvent (int howMany) {
+/*void receiveEvent (int howMany) {
   while(Wire.available() > 0) {
     char messageType = Wire.receive();
     int n = Wire.receive();
@@ -55,10 +74,54 @@ void receiveEvent (int howMany) {
     int c = Wire.receive();
     
     if(messageType == 'a' && midiStatus == true) {
-      if(n == 30)
-        waveform0.frequency(360 * vp + 0.25);
+      if(n == 30) {
+        waveform7.frequency(360 * vp + 0.25);
       }
-  
+      if(n == 31){
+        waveform1.frequency(360 * vp + 0.25);
+      }
+      if(n == 32) {
+        waveform2.frequency(360 * vp + 0.25);
+      }
+      if(n == 33) {
+        waveform3.frequency(360 * vp + 0.25);
+      }
+      if(n == 34) {
+        waveform4.frequency(360 * vp + 0.25);
+      }
+      if(n == 35) {
+        waveform5.frequency(360 * vp + 0.25);
+      }
+      if(n == 36) {
+        waveform6.frequency(360 * vp + 0.25);
+      }
+      if(n == 37) {
+        waveform8.frequency(360 * vp + 0.25);
+      }
+      if(n == 38) {
+        waveform1.amplitude(360 * vp + 0.25);
+      }
+      if(n == 39) {
+        waveform2.amplitude(360 * vp + 0.25);
+      }
+      if(n == 40) {
+        waveform3.amplitude(360 * vp + 0.25);
+      }
+      if(n == 41) {
+        waveform4.amplitude(360 * vp + 0.25);
+      }
+      if(n == 42) {
+        waveform5.amplitude(360 * vp + 0.25);
+      }
+      if(n == 43) {
+        waveform6.amplitude(360 * vp + 0.25);
+      }
+      if(n == 44) {
+        waveform8.amplitude(360 * vp + 0.25);
+      }
+      if(n == 45) {
+        waveform7.amplitude(360 * vp + 0.25);
+      }
       delay(5);
     }
     if(messageType == 'n' && midiStatus == true) {
@@ -70,7 +133,7 @@ void receiveEvent (int howMany) {
       delay(5);
     }
     delay(5);
-  } 
+  }
   usbMIDI.send_now();
-}
+}*/
 
--- Arduino/synthmaster/synthmaster.ino
+++ Arduino/synthmaster/synthmaster.ino
@@ -1,54 +1,75 @@
+#include <Audio.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioPlayMemory          playMem3;       //xy=144.00000762939453,232.00000762939453
+AudioPlayMemory          playMem4;       //xy=164.00000762939453,302.00000953674316
+AudioPlayMemory          playMem1;       //xy=167,70
+AudioPlayMemory          playMem2;       //xy=169.00000381469727,144.00000476837158
+AudioMixer4              mixer1;         //xy=415,124
+AudioOutputAnalog        dac1;           //xy=719,139
+AudioConnection          patchCord1(playMem3, 0, mixer1, 2);
+AudioConnection          patchCord2(playMem4, 0, mixer1, 3);
+AudioConnection          patchCord3(playMem1, 0, mixer1, 0);
+AudioConnection          patchCord4(playMem2, 0, mixer1, 1);
+AudioConnection          patchCord5(mixer1, dac1);
+// GUItool: end automatically generated code
+
 
 boolean midiStatus = false;
 int counter = 1;
 int previousCounter = 1;
 long previousMillis = 0;
-boolean samplePlayed = false;
 
 int switchStatus[] =  {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+int samplePlaying[] = {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
 int switchChannel[] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
 int switchTiming[] =  {1,  1,  1,  1,  2,  2,  2,  2,  3,  3,  3,  3,  4,  4,  4,  4};
 
-long interval = 1000;
+long intervalX = 1000;
 
 void setup() {
   pinMode(13, OUTPUT);
+  //digitalWrite(13, HIGH);
   delay(150);
   Wire.begin(5);
   Wire.onReceive(receiveEvent);
   delay(1000);
+  Serial.print(counter);
+  delay(1000);
+  Serial.print(previousCounter);
 }
 
 void loop() {
+  Serial.print(counter);
+
   unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis > interval) {
+  if(currentMillis - previousMillis > intervalX) {
     previousMillis = currentMillis;
     previousCounter = counter;
-    if(previousCounter == 4) {
+    if(previousCounter > 3) {
       counter = 1;
     } else {
-      counter = previousCounter + 1;
+      counter = counter + 1;
+      digitalWrite(13, LOW);
     }
   }
-
-  
-  //PLAY SAMPLES
-  for(int i=0; i<17; i++){
-    if(counter == switchTiming[i] && samplePlayed == false) {
-      if(switchStatus[i] > 0) {
-        //play audio sample
-        digitalWrite(13, HIGH);
-        samplePlayed = true;
-      } else {
-        digitalWrite(13, LOW);
-        samplePlayed = false;
-      }
+  for(int i=0; i<17; i++) {
+    if(switchTiming[i] == counter && switchStatus[i] > 0) {
+      //play(//data);
+      Serial.print("playing");
+      switchStatus[i] = 0;
     }
   }
 }
+
+
+  
 void receiveEvent (int howMany) {
-  while(Wire.available() > 0) {
+  if(Wire.available() > 0) {
     char messageType = Wire.receive();
     int n = Wire.receive();
     int vp = Wire.receive();
@@ -59,22 +80,23 @@ void receiveEvent (int howMany) {
       delay(5);
     }
     if(messageType == 'n' && midiStatus == false) {
-      for(int i=0; i<17; i++){
-        if(switchChannel[i] = n){
-          switchStatus[i] = 1;
+      for(int s=0; s<17; s++){
+        if(switchChannel[s] = n){
+          switchStatus[s] = 1;
         }
       }
       delay(5);
     }
-    if(messageType == 'o' && midiStatus == false) {
-      for(int i=0; i<17; i++){
-        if(switchChannel[i] = n){
-          switchStatus[i] = 0;
+    if(messageType == 'o' && midiStatus == true) {
+      for(int p=0; p<17; p++){
+        if(switchChannel[p] = n){
+          switchStatus[p] = 0;
         }
       }
+      delay(5);
+    }
     delay(5);
-  }
   } 
   usbMIDI.send_now();
-}
 
+}
